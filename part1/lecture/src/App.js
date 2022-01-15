dd3da4eec28e81c031b4b3ecff7977a7b48fdfe2
lionelyau23 <lionel1997613@gmail.com>
2022-01-15 16:12:04 +0800

    part c stateful component

--- part1/lecture/src/App.js
+++ part1/lecture/src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = (props) => {
-    const {counter} = props
+    const [ counter, setCounter ] = useState(0)
+
+    setTimeout(
+        () => setCounter(counter + 1),
+        1000
+    )
 
     return (
         <div>{counter}</div> 
     )
 }
-
 
 export default App
