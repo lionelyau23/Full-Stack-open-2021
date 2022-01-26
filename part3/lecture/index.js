2fc33faf4772a37937366addcd1d36cc77a16fd0
lionelyau23 <lionel1997613@gmail.com>
2022-01-26 17:05:08 +0800

    completed part 3 c before ex 3.13

--- part3/lecture/index.js
+++ part3/lecture/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const app = express()
 
-// asd
+const Note = require('./models/note')
 
 let notes = [  
     {   
@@ -42,19 +44,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(result => {
+        response.json(result)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.statusMessage = 'no such note id exists'
-        response.status(404).end()
-    }
+    })
 })
 
 const generateId = () => {
@@ -74,15 +72,15 @@ app.post('/api/notes', (request, response) => {
         })
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
         important: body.important || false,
-        date: new Date(),
-        id: generateId()
-    }
+        date: new Date()
+    })
 
-    notes = notes.concat(note)
-    response.json(note)
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -109,6 +107,6 @@ const unknownEndPoint = (request, response) => {
 
 app.use(unknownEndPoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
