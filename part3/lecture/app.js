5272a895cf761a62df0605d8a01274f56d2ca812
lionelyau23 <lionel1997613@gmail.com>
2022-01-28 20:36:21 +0800

    completed part 4a before ex 4.1

---
+++ part3/lecture/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const noteRouter = require('./controllers/notes')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const middleWare = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+logger.info(`Connecting to ${config.MONGODB_URI}`)
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch(error => {
+        logger.error('error connecting to MongoDB', error.message)
+    })
+
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleWare.requestLogger)
+
+app.use('/api/notes', noteRouter)
+
+app.use(middleWare.unknownEndPoint)
+app.use(middleWare.errorHandler)
+
+module.exports = app
--- part3/lecture/models/note.js
+++ part3/lecture/models/note.js
@@ -2,16 +2,6 @@ const mongoose = require('mongoose')
 const logger = require('../utils/logger')
 const config = require('../utils/config')
 
-const url = config.MONGODB_URI
-
-mongoose.connect(url)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch(error => {
-        logger.error('error connecting to MongoDB', error.message)
-    })
-
 const noteSchema = new mongoose.Schema({
     content: {
         type: String,
