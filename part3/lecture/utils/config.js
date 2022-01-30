7a6df09ba00f5e1355cd3606497df7159b585a29
lionelyau23 <lionel1997613@gmail.com>
2022-01-30 18:54:43 +0800

    completed part4b before ex 4.8

--- part3/lecture/utils/config.js
+++ part3/lecture/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.MONGOATLAS_URL
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGOATLAS_URL
+    : process.env.MONGOATLAS_URL
 
 module.exports = {
     PORT,
