f256947bd3894adcd1219d497d7f80ff000b0bfe
lionelyau23 <lionel1997613@gmail.com>
2022-02-02 17:11:45 +0800

    completed ex 4.17

--- part4/bloglist/controller/blogs.js
+++ part4/bloglist/controller/blogs.js
@@ -1,23 +1,33 @@
 // const logger = require('../utils/logger')
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('user', { username: 1, name: 1, id: 1 })
     response.json(blogs)
 })
 
 blogRouter.post('/', async (request, response) => {
     const body = request.body
+    const user = (await User.find({}))[0]
+    console.log(user)
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes || 0
+        likes: body.likes || 0,
+        user: user._id
     })
 
     const result = await blog.save()
+
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
+
     response.status(201).json(result)
 })
 
